ecbf81ae2e8b246e397d3bb53abb2ea517cd3989
Paul Hunt <paul@hairybeast>
2013-11-27 00:08:06 +0000

    Fixed csrf

--- payments/multipayments/ipn.py
+++ payments/multipayments/ipn.py
@@ -56,83 +56,45 @@ Then in urls.py:
 """
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
-from django.views.generic import TemplateView
 import urllib
+from mezzanine.utils.importing import import_dotted_path
 from mezzanine.conf import settings
 
+handler = lambda s: import_dotted_path(s) if s else lambda *args: None
 
-class Endpoint(TemplateView):
-    
-    default_response_text = 'Nothing to see here'
-    verify_url = settings.PAYPAL_SUBMIT_URL
-    
-    @method_decorator(csrf_exempt)
-    def dispatch(self, *args, **kwargs):
-        return super(EndPoint, self).dispatch(*args, **kwargs)
-        
-        
-    def do_post(self, url, args):
-        return urllib.urlopen(url, urllib.urlencode(args)).read()
-    
-    def verify(self, data):
-        args = {
-            'cmd': '_notify-validate',
-        }
-        args.update(data)
-        return self.do_post(self.verify_url, args) == 'VERIFIED'
-    
-    def default_response(self):
-        return HttpResponse(self.default_response_text)
-    
-    
-    def __call__(self, request, uuid):
-        r = None
-        #print "UUID:",uuid
-        #data = dict(request.GET.items())
-        #r = self.process(data, uuid)
-        
-        
-        if request.method == 'POST':
-            
-            ipn_handler = settings.ORDER_IPN_HANDLER_CLASS()
-            
-            data = dict(request.POST.items())
-            # We need to post that BACK to PayPal to confirm it
-            if self.verify(data):
-                r = ipn_handler.process(data, uuid)
-            else:
-                r = ipn_handler.process_invalid(data, uuid)
-        if r:
-            return r
+ipn_handler = handler(settings.ORDER_IPN_HANDLER_CLASS)()
+
+@csrf_exempt
+def _do_post(url, args):
+    return urllib.urlopen(url, urllib.urlencode(args)).read()
+
+
+@csrf_exempt
+def _verify(data):
+    args = { 'cmd': '_notify-validate' }
+    args.update(data)
+    return _do_post(settings.PAYPAL_SUBMIT_URL, args) == 'VERIFIED'
+
+
+@csrf_exempt
+def EndPoint(request, uuid):
+    r = None
+    data = dict(request.POST.items())
+    ipn_handler.process(data, uuid)
+
+    if request.method == 'POST':
+        print "GOT POST"    
+        data = dict(request.POST.items())
+        print "GOT DATA AS DICT"
+        # We need to post that BACK to PayPal to confirm it
+        if _verify(data):
+            print "VERIFIED"
+            r = ipn_handler.process(data, uuid)
         else:
-            return self.default_response()
-    
-    def process(self, data):
-        pass
-    
-    def process_invalid(self, data):
-        pass
+            print "NOT VERIFIED"
+            r = ipn_handler.process_invalid(data, uuid)
+    if r:
+        return r
+    return HttpResponse('Nothing to see here')
 
-        
 
-#class CartidgePaypalIpnEndpoint(Endpoint):
-    #def process(self, data, uuid):
-        ## Do something with valid data from PayPal - e-mail it to yourself,
-        ## stick it in a database, generate a license key and e-mail it to the
-        ## user... whatever
-        #new_order = Order.objects.filter(callback_uuid=uuid)
-        #if order:
-            #o = OrderReady(order=new_order)
-            #o.save()
-        #else:
-            #o = OrderFailed(reason="Order not found", uuid=uuid)
-            #o.save()
-    
-    
-    #def process_invalid(self, data, uuid):
-        ## Do something with invalid data (could be from anywhere) - you 
-        ## should probably log this somewhere
-        #o = OrderFailed(reason="Could not verify", uuid=uuid)
-        #o.save()
-    
